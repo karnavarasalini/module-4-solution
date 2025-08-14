25d50c324581268e82e640aa3ff26c2e6d8e795c
karnavarasalini <karnavarasalini@gmail.com>
2025-08-14 16:19:02 +0530

    Create SpeakHello.js

---
+++ SpeakHello.js
@@ -0,0 +1,11 @@
+// Wrap in IIFE to avoid polluting global scope
+(function (window) {
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
